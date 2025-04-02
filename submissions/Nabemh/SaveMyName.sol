a2d6ac842f4994fc149d099c61cce02992903792
Nabemh <nhanior@gmail.com>
2025-04-02 10:29:40 +0100

    fix: mapping

--- submissions/Nabemh/SaveMyName.sol
+++ submissions/Nabemh/SaveMyName.sol
@@ -3,5 +3,14 @@
 pragma solidity ^0.8.0;
 
 contract saveData {
-    
+    struct User{
+        string name;
+        string bio;
+    }
+
+    mapping (address => User) public profile;
+
+    function createProfile(string memory _name, string memory _bio) public {
+        
+    }
 }
