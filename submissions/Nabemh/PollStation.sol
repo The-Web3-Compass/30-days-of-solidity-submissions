0cd7f96408fdc6e41d812c9970c99503c79a33a5
Nabemh <nhanior@gmail.com>
2025-04-12 09:33:46 +0100

    fix: Redo PollStation using mapping, removing struct

--- submissions/Nabemh/PollStation.sol
+++ submissions/Nabemh/PollStation.sol
@@ -1,27 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-contract PollStation {
-    // defining a candidate using structs
-    struct Candidate {
-        uint[] id;
-        uint idVotes;
+pragma solidity ^0.8.9;
+
+contract PollStation{
+    string[] public candidate;
+
+    mapping (string => uint256) votes;
+
+    function register(string memory _candidate) public {
+        candidate.push(_candidate);
+        votes[_candidate] = 0;
     }
 
-    mapping(address => uint) public voter;
-    Candidate[] public candidates; //array for all candidates
-
-    function addCandidate(uint _id) public {
-        Candidate storage newCandidate = candidates.push();
-        newCandidate.id.push(_id);  // adding the id
-        newCandidate.idVotes = 0;
+    function vote(string memory _candidate) public {
+        votes[_candidate]++;
     }
 
-    function castVote(uint _i) public {
-        candidates[_i].idVotes++;
+    function getCandidates() public view returns (string[] memory){
+        return (candidate);
     }
 
-    function getVotes(uint _i) public view returns (uint) {
-        return candidates[_i].idVotes; 
+    function getVotes(string memory _candidate) public view returns (uint256) {
+        return (votes[_candidate]);
     }
 }
