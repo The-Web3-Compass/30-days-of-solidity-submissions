1ed7cd6027dcce578c2d51f8ebc241bd6526c1d6
loki <lokeshyadav1098@gmail.com>
2025-05-26 01:29:33 +0530

    30 days challenge complete

--- submissions/Lokii0007/day30-miniDexFactory.sol
+++ submissions/Lokii0007/day30-miniDexFactory.sol
@@ -18,20 +18,20 @@ contract miniDexFactory is Ownable {
 
     constructor(address _owner) Ownable(_owner) {}
 
-    function createPair(address _tokenA, address _tokenB) external onlyOwner {
+    function createPair(address _tokenA, address _tokenB) external onlyOwner returns(address pair) {
         require(_tokenA != _tokenB, "identical tokens");
         require(
             _tokenA != address(0) && _tokenB != address(0),
             "invalid tokens"
         );
-        require(getPair[_tokenA][_tokenB], "token already exist");
+        require(getPair[_tokenA][_tokenB] == address(0), "token already exist");
 
         (address token0, address token1) = _tokenA < _tokenB
             ? (_tokenA, _tokenB)
             : (_tokenB, _tokenA);
         pair = address(new MiniDex(token0, token1));
         getPair[token1][token0] = pair;
-        getPair[token1 = 0][token1] = pair;
+        getPair[token0][token1] = pair;
 
         allPairs.push(pair);
 
