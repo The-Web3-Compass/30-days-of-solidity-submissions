b0f271bcbfa34f8260105419bc54adcbcf683c3a
chao1717 <woodsmanuelc@gmail.com>
2025-06-14 12:05:45 +0800

    Update day9-smartcalculator1.sol

--- submissions/colacolaTest/day9-smartcalculator1.sol
+++ submissions/colacolaTest/day9-smartcalculator1.sol
@@ -1,8 +1,7 @@
 //SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-import "./day09-ScientificCalculator0.sol";
+import "./day9-ScientificCalculator.sol";
 
 contract Calculator{
 
@@ -23,46 +22,34 @@ contract Calculator{
         }
 
     function add(uint256 a, uint256 b)public pure returns(uint256){
-        uint256 result = a+b;
-        return result;
+       return (a+b);
     }
 
     function subtract(uint256 a, uint256 b)public pure returns(uint256){
-        uint256 result = a-b;
-        return result;
+         return (a-b);
     }
 
     function multiply(uint256 a, uint256 b)public pure returns(uint256){
-        uint256 result = a*b;
-        return result;
+        return (a*b);
     }
 
     function divide(uint256 a, uint256 b)public pure returns(uint256){
         require(b!= 0, "Cannot divide by zero");
-        uint256 result = a/b;
-        return result;
+        return (a/b);
     }
 
     function calculatePower(uint256 base, uint256 exponent)public view returns(uint256){
-
-    ScientificCalculator scientificCalc = ScientificCalculator(scientificCalculatorAddress);
-
-    //external call 
-    uint256 result = scientificCalc.power(base, exponent);
-
-    return result;
-
-}
+        ScientificCalculator scientificCalc = ScientificCalculator(scientificCalculatorAddress);
+        uint256 result = scientificCalc.power(base, exponent);
+        return result;
+   }
 
     function calculateSquareRoot(uint256 number)public returns (uint256){
-        require(number >= 0 , "Cannot calculate square root of negative nmber");
 
-        bytes memory data = abi.encodeWithSignature("squareRoot(int256)", number);
+        bytes memory data = abi.encodeWithSignature("squareRoot(uint256)", number);
         (bool success, bytes memory returnData) = scientificCalculatorAddress.call(data);
         require(success, "External call failed");
         uint256 result = abi.decode(returnData, (uint256));
         return result;
-    }
-
-    
+    }  
 }
