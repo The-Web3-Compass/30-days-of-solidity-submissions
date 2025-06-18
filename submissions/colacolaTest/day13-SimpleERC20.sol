7212d46de2c4ced3e234b951f1d74a1f446c8d51
chao1717 <woodsmanuelc@gmail.com>
2025-06-18 20:52:14 +0800

    Update day13-SimpleERC20.sol

--- submissions/colacolaTest/day13-SimpleERC20.sol
+++ submissions/colacolaTest/day13-SimpleERC20.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.8.20;
 
-contract SimpleERC20 {
+contract SimpleERC20{
     string public name = "SimpleToken";
     string public symbol = "SIM";
     uint8 public decimals = 18;
@@ -13,37 +13,45 @@ contract SimpleERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(uint256 _initialSupply) {
-        totalSupply = _initialSupply * (10 ** uint256(decimals));
+    constructor(uint256 initialSupply) {
+        totalSupply = initialSupply * (10 ** uint256(decimals));
         balanceOf[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function transfer(address _to, uint256 _value) public virtual returns (bool) {
-        require(balanceOf[msg.sender] >= _value, "Not enough balance");
-        _transfer(msg.sender, _to, _value);
+    function transfer(address _to, uint256 value) public virtual returns(bool){
+        require(value > 0, "Invalid amount");
+        _transfer(msg.sender, _to, value);
+
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
+    function approve(address _spender, uint256 value) public returns(bool){
+        require(_spender != address(0), "Invalid address");
+        require(value > 0, "Invalid amount");
+
+        allowance[msg.sender][_spender] = value;
+
+        emit Approval(msg.sender, _spender, value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool) {
-        require(balanceOf[_from] >= _value, "Not enough balance");
-        require(allowance[_from][msg.sender] >= _value, "Allowance too low");
+    function transferFrom(address _from, address _to, uint256 value) public virtual returns(bool){
+        require(value <= allowance[_from][msg.sender], "Not enough allowance");
+        _transfer(_from, _to, value);
+        allowance[_from][msg.sender] -= value;
 
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
         return true;
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0), "Invalid address");
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
+    function _transfer(address _from, address _to, uint256 value) internal {
+        require(_from != address(0), "Invalid address");
+        require( _to != address(0), "Invalid address");
+        require(value > 0, "Invalid amount");
+        require(value <= balanceOf[_from], "Not enough balance");
+
+        balanceOf[_from] -= value;
+        balanceOf[_to] += value;
+        emit Transfer(_from, _to, value);
     }
 }
