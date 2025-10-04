cde37bb5b02936c39c4e074799ee9ba344ce3bd9
CeciliaOrlando <168267241+CeciliaOrlando@users.noreply.github.com>
2025-10-04 05:31:56 -0300

    Update challenge title from Day 3 to Day 4

--- submissions/CeciliaOrlando/04-AuctionHouse.sol
+++ submissions/CeciliaOrlando/04-AuctionHouse.sol
@@ -1,4 +1,4 @@
-# Desafío del Día 3 - Solución de CeciliaOrlando
+# Desafío del Día 4 - Solución de CeciliaOrlando
 
 ## 🔗 Enlace al Repositorio Completo
 https://github.com/CeciliaOrlando/04-AuctionHouse.sol
