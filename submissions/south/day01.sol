e7e600106b294f7a1e4f3854b87dd75e1c554877
nanshaobin <nanshaobin@gmail.com>
2025-10-13 16:04:30 +0800

    added day 01

---
+++ submissions/south/day01.sol
@@ -0,0 +1,13 @@
+// SPDx-Lincense-Identifier;MIT
+
+pragma solidity ^0.8.0;
+
+contract clickcounter{
+
+    unit256 public counter ;
+
+    function click() public
+    {
+        counter ++;
+         }
+}
