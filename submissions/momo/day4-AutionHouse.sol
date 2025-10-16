16dd90e2b734e6647798e052b143c45ca1f0eb21
Octopus <99575402+a-god-of-death@users.noreply.github.com>
2025-10-16 21:12:46 +0800

    Update day4-AutionHouse.sol

--- submissions/momo/day4-AutionHouse.sol
+++ submissions/momo/day4-AutionHouse.sol
@@ -1,53 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AutionHouse {
+contract AuctionHouse {
   address public owner;
 
   string public item;
   uint public startingPrice;
-  uint public autionEndTime;
+  uint public auctionEndTime;
 
   address private highestBidder;
   uint private highestBid;
 
   
 
-  mapping(adress => uint) bids;
+  mapping(address => uint) bids;
 
   address[] public  bidders;
 
-  bool pubic contractActive = true;
+  bool public contractActive = true;
 
-  event contractDisabled(address, amount);
+  event contractDisabled(address, uint);
 
-  modifier(){
-    require(contractActive，“Contract has closed.”);
-    _;
-  }
   
-  construtor(string _item, uint bid_time )
+  constructor(string memory _item, uint bid_time )
   {
     owner = msg.sender;
     item = _item;
-    autionEndTime = block.timestamp + bid_time;
+    auctionEndTime = block.timestamp + bid_time;
   }
 
   function bid(uint _amount) external {
     require(autionEndTime > block.timestamp,"Aution has ended.");
     require(_amount>startingPrice, "The aution amount has greater than starting price.");
-    require(bids[msg.sender] * 105 / 100 <= _amount, "Must greater than your last bid‘s 5%");
+    require(bids[msg.sender] * 105 / 100 <= _amount, "Must be greater than your last bid`s 5%");
     
 
     if(bids[msg.sender] == 0){
-      bidders.push(msg.sender)
+      bidders.push(msg.sender);
     }
-    bids[sender] = _amount;
+    bids[msg.sender] = _amount;
 
     if(_amount > highestBid) {
       highestBid = _amount;
       highestBidder = msg.sender;
-    }
+    }}
 
     function endBid() external {
       require(block.timestamp >= autionEndTime);
@@ -59,4 +55,6 @@ contract AutionHouse {
     function getBidders() external view returns(address[] memory){
       return bidders;
     }    
+
+    
   }
