3c5feb36e034406c41bbba7f7a32c3a9986bdc39
chenqing0106 <1065255262@qq.com>
2025-10-20 15:01:03 +0800

    day1：计时器counter

--- submissions/SylvaQing/day1.sol
+++ submissions/SylvaQing/day1.sol
@@ -1,1 +1,10 @@
-//test
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract ClickCounter {
+    uint256 public counter;
+    function click() public{
+        counter++;
+    }
+}
+
