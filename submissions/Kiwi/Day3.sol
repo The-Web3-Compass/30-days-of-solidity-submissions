746e8c4078122080f58c43fc5cf0b2eed5aa50dd
Kiwi <98079592+SHIEREN@users.noreply.github.com>
2025-10-22 06:24:57 +0000

    day3

---
+++ submissions/Kiwi/Day3.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.0;
+
+contract PollStation{
+    string [] public candidatenames;
+    mapping (string => uint256) voteCount;
+
+    function addCandidateNames (string memory _candidateNames) public {
+        
+        candidatenames.push(_candidateNames);
+        voteCount[_candidateNames] = 0;
+
+    }
+    
+    function getCandidateNames () public view returns(string[] memory){
+              return candidatenames;
+    }
+
+    function vote(string memory _candidateNames) public {
+        voteCount[_candidateNames] ++;
+    }
+
+    function getVote (string memory _candidateNames) public view returns(uint256) {
+             
+             return voteCount[_candidateNames];
+    }
+}
+
