67255155e95230de952295332b5a8518f6280b79
doubleqian888 <yjqqian0420@gmail.com>
2025-10-22 14:43:08 +0000

    renew a solidity version 3

---
+++ submissions/doubleqian/day3.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PollStation {
+    string[] public candidateNames;
+    mapping(string => uint256) public voteCount;
+
+    function addCandidateNames(string memory _candidateName) public {
+        candidateNames.push(_candidateName);
+        voteCount[_candidateName] = 0;
+    }
+
+    function getCandidateNames() public view returns (string[] memory) {
+        return candidateNames;
+    }
+
+    function vote(string memory _candidateName) public {
+        voteCount[_candidateName] += 1;
+    }
+
+    function getVote(string memory _candidateName) public view returns (uint256) {
+        return voteCount[_candidateName];  // Fixed: Changed __candidateNames → _candidateName
+    }
+}
