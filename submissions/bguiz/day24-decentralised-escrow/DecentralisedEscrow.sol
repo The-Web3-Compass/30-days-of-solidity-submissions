e81da96f01e2c8bf356e723b3534ba34bce7a1a3
Brendan Graetz <bguiz@users.noreply.github.com>
2025-10-25 00:16:36 +0800

    feat: impl decentralised escrow for day 24
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

---
+++ submissions/bguiz/day24-decentralised-escrow/DecentralisedEscrow.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.30;
+
+/**
+ * @title DecentralisedEscrow
+ * @dev Build a secure system for holding funds until conditions are met.
+ * You'll learn how to manage payments and handle disputes.
+ * It's like a digital middleman for secure transactions, demonstrating secure conditional payments.
+ * implementation of https://www.web3compass.xyz/challenge-calendar day 24
+ */
+contract DecentralisedEscrow {
+}
