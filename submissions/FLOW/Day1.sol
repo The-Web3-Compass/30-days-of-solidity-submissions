8095f259afde815a83b7b4f2fe4ffd0d2bea5605
lintian31 <1771289423@qq.com>
2025-10-26 14:20:40 +0800

    feat: Day01 提交 by FLOW

---
+++ submissions/FLOW/Day1.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ClickCounter {
+    // 状态变量 - 存储点击次数
+    uint256 public counter;
+
+    // 函数 - 增加计数器
+    function click() public {
+        counter++;
+    }
+
+    // 函数 - 实现reset功能
+    function reset() public {
+        counter = 0;
+    }
+}
