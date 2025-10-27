4b40b758879bd172741987530c26db525f669ede
iqnuxul <mluxunqi@gmail.com>
2025-10-27 22:38:11 +0000

    day 6 submission update - Added 2 different withdrawal function that sends Ether back to users

--- submissions/xunqilu/day6.sol
+++ submissions/xunqilu/day6.sol
@@ -14,40 +14,79 @@ contract EtherPiggyBank{
     }
 
     modifier onlyBankManager(){
-        require(msg.sender == bankManager, "Only bank manager can perform this action");
+        require(msg.sender == bankManager, "You are not allowed to do it");
         _;
     }
 
     modifier onlyRegisteredMember() {
-        require(registeredMembers[msg.sender], "Member not registered");
+        require(registeredMembers[msg.sender], "Member not found");
         _;
     }
   
     function addMembers(address _member)public onlyBankManager{
         require(_member != address(0), "Invalid address");
-        require(_member != msg.sender, "Bank Manager is already a member");
+        require(_member != msg.sender, "Your are already registered");
         require(!registeredMembers[_member], "Member already registered");
         registeredMembers[_member] = true;
         members.push(_member);
     }
 
-    function getMembers() public view returns(address[] memory){
+    function getMembers() public view returns(address[] memory){ // when we defined a array, we need to add memory
         return members;
     }
     
-    //deposit in Ether
-    function depositAmountEther() public payable onlyRegisteredMember{  
+    //this piece code won't really change the ether balance
+    function deposit(uint256 _amount) public onlyRegisteredMember{
+        require(_amount > 0, "Invalid amount");
+        balance[msg.sender] += _amount;
+    }
+
+
+    //deposit in Ether using payable and msg.value
+    function depositEther() public payable onlyRegisteredMember{  
         require(msg.value > 0, "Invalid amount");
-        balance[msg.sender] = balance[msg.sender]+msg.value;
+        balance[msg.sender] += msg.value;
    
     }
     
+    //1 Ether = 10^18 Wei
+
+    //didn't withdraw real ether from the address
+    // only modified the mapping inside the contract
     function withdrawAmount(uint256 _amount) public onlyRegisteredMember{
         require(_amount > 0, "Invalid amount");
         require(balance[msg.sender] >= _amount, "Insufficient balance");
-        balance[msg.sender] = balance[msg.sender]-_amount;
+        balance[msg.sender] -= _amount;
    
     }
+
+    // METHOD 1 - withdraw the real ether 
+    //amount unit in Wei
+    function withdrawEther1(uint256 _amount) public onlyRegisteredMember {
+        require(_amount > 0, "Invalid amount");
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+
+        // update balance first
+        balance[msg.sender] -= _amount;
+        // transfer
+        payable(msg.sender).transfer(_amount);
+}
+
+
+    // METHOD - 2 withdraw the real ether 
+    //amount unit in Wei
+    // chatgpt said it's safer way and no gas limit， where method 1 has 2300 gas limit？
+    //still trying to understand it
+    function withdrawEther2(uint256 _amount) public onlyRegisteredMember {
+        require(_amount > 0, "Invalid amount");
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+
+        balance[msg.sender] -= _amount;
+
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "Transfer failed");
+    }
+
 
     function getBalance(address _member) public view returns (uint256){
         require(_member != address(0), "Invalid address");
