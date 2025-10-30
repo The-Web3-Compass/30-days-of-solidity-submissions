6e2f5048984d81ae899e7240efbca3a27901f33f
Little8monster <jolinjiang8@gmail.com>
2025-10-30 19:29:36 +0800

    修改标题

--- submissions/Little8Monster/Day09-Calculator.sol
+++ submissions/Little8Monster/Day09-Calculator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./Day9-ScientificCalculator.sol";
+import "./Day09-ScientificCalculator.sol";
 
 contract Calculator{
     address public owner;
