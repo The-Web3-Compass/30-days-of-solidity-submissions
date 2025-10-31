8699084ac7cabf305f8bac8fd05b6442aa16fb17
mokching777 <mokching57@gmail.com>
2025-10-31 14:34:09 +0800

    Day11-VaultMaster

---
+++ Mokching777/Day11-VaultMaster.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VaultMaster is Ownable{
+    
+    event DepositSuccessful(address indexed account,uint256 value);
+    event WithdrawSuccessful(address indexed recipient,uint256 value);
+
+    constructor() Ownable(msg.sender) {}
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0.");
+        emit DepositSuccessful(msg.sender, msg.value);
+    }
+
+    function withdraw(address _to,uint256 _amount) public onlyOwner{
+        require(_amount <= getBalance(),"Insufficient balance.");
+        (bool success , ) = payable (_to).call{value:_amount}("");
+        require(success,"Transfer Failed.");
+        emit WithdrawSuccessful(_to, _amount);
+    }
+
+    function transferOwnership(address newOwner) public override onlyOwner {
+    require(newOwner != address(this), "Contract can't own itself");
+    super.transferOwnership(newOwner);
+}
+}
