df4c9ed1dbdd0a739389bfb38bb37b8ea60b154f
Serena76 <zhchunyuu@gmail.com>
2025-11-18 00:21:19 +0800

    修改

--- submissions/SerenaTest/Day25/AutomatedMarketMaker.sol
+++ submissions/SerenaTest/Day25/AutomatedMarketMaker.sol
@@ -7,7 +7,8 @@ contract AutomatedMarketMaker is ERC20 {
     IERC20 public tokenA;
     IERC20 public tokenB;
 
-    uint256 public reserveA;
+    //两种代币数量
+    uint256 public reserveA;  
     uint256 public reserveB;
 
     address public owner;
@@ -16,12 +17,32 @@ contract AutomatedMarketMaker is ERC20 {
     event LiquidityRemoved(address indexed provider, uint256 amountA, uint256 amountB, uint256 liquidity);
     event TokensSwapped(address indexed trader, address tokenIn, uint256 amountIn, address tokenOut, uint256 amountOut);
 
+    //LP代币的名称和符号是利用ERC20的构造器完成的
     constructor(address _tokenA, address _tokenB, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         tokenA = IERC20(_tokenA);
         tokenB = IERC20(_tokenB);
         owner = msg.sender;
     }
 
+     function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    //巴比伦平方根算法
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    //用户向池子里添加A B代币能够获得LP代币
     function addLiquidity(uint256 amountA, uint256 amountB) external {
         require(amountA > 0 && amountB > 0, "Amounts must be > 0");
 
@@ -29,23 +50,24 @@ contract AutomatedMarketMaker is ERC20 {
         tokenB.transferFrom(msg.sender, address(this), amountB);
 
         uint256 liquidity;
-        if (totalSupply() == 0) {
+        if (totalSupply() == 0) {  //第一个添加流动性的人  直接开方
             liquidity = sqrt(amountA * amountB);
         } else {
-            liquidity = min(
+            liquidity = min(    //后面的人要根据最少的代币铸造LP代币
                 amountA * totalSupply() / reserveA,
                 amountB * totalSupply() / reserveB
             );
         }
 
-        _mint(msg.sender, liquidity);
+        _mint(msg.sender, liquidity);  //铸造LP代币发送给用户
 
         reserveA += amountA;
         reserveB += amountB;
 
         emit LiquidityAdded(msg.sender, amountA, amountB, liquidity);
     }
-
+    
+    //提取  按照此时池子里面代币占比乘LP份额得出实际代币
     function removeLiquidity(uint256 liquidityToRemove) external returns (uint256 amountAOut, uint256 amountBOut) {
         require(liquidityToRemove > 0, "Liquidity to remove must be > 0");
         require(balanceOf(msg.sender) >= liquidityToRemove, "Insufficient liquidity tokens");
@@ -61,7 +83,7 @@ contract AutomatedMarketMaker is ERC20 {
         reserveA -= amountAOut;
         reserveB -= amountBOut;
 
-        _burn(msg.sender, liquidityToRemove);
+        _burn(msg.sender, liquidityToRemove);  //销毁LP代币
 
         tokenA.transfer(msg.sender, amountAOut);
         tokenB.transfer(msg.sender, amountBOut);
@@ -70,14 +92,16 @@ contract AutomatedMarketMaker is ERC20 {
         return (amountAOut, amountBOut);
     }
 
+     //用A换B
     function swapAforB(uint256 amountAIn, uint256 minBOut) external {
         require(amountAIn > 0, "Amount must be > 0");
         require(reserveA > 0 && reserveB > 0, "Insufficient reserves");
 
+         //收取服务费
         uint256 amountAInWithFee = amountAIn * 997 / 1000;
         uint256 amountBOut = reserveB * amountAInWithFee / (reserveA + amountAInWithFee);
 
-        require(amountBOut >= minBOut, "Slippage too high");
+        require(amountBOut >= minBOut, "Slippage too high");   //用户期望用amountAIn的A代币换取minBOut的B代币  如果达不到就不换
 
         tokenA.transferFrom(msg.sender, address(this), amountAIn);
         tokenB.transfer(msg.sender, amountBOut);
@@ -106,24 +130,11 @@ contract AutomatedMarketMaker is ERC20 {
         emit TokensSwapped(msg.sender, address(tokenB), amountBIn, address(tokenA), amountAOut);
     }
 
+
+    //池子状态
     function getReserves() external view returns (uint256, uint256) {
         return (reserveA, reserveB);
     }
 
-    function min(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a < b ? a : b;
-    }
-
-    function sqrt(uint256 y) internal pure returns (uint256 z) {
-        if (y > 3) {
-            z = y;
-            uint256 x = y / 2 + 1;
-            while (x < z) {
-                z = x;
-                x = (y / x + x) / 2;
-            }
-        } else if (y != 0) {
-            z = 1;
-        }
-    }
+    
 }
