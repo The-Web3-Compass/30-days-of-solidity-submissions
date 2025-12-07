3b29a66eeeb50896d4c128ec14101759347eb661
iqnuxul <mluxunqi@gmail.com>
2025-12-07 21:54:58 +0100

    day 13 submission by xunqi

--- submissions/xunqilu/day12/MyToken.sol
+++ submissions/xunqilu/day12/MyToken.sol
@@ -28,13 +28,13 @@ contract MyToken{
         emit Transfer(_from, _to, _value);
     }
 
-    function transfer(address _to, uint256 _value)public returns (bool success){
+    function transfer(address _to, uint256 _value)public virtual returns (bool success){
         require(balanceOf[msg.sender]>= _value, "Insufficient balance");
         _transfer(msg.sender, _to, _value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value)public returns(bool success){
+    function transferFrom(address _from, address _to, uint256 _value)public virtual returns(bool success){
         require(balanceOf[_from] >= _value,"Insufficient balance");
         require(allowance[_from][msg.sender] >= _value,"Not enough allowence");
         allowance[_from][msg.sender]-= _value;
