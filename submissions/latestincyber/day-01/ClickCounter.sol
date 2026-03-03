f6a20f3b585876dcf212823a6beeb93d2198a3b8
doryoysterpie <ilbricconemarketing@gmail.com>
2026-03-03 12:30:05 -0500

    Day 01 Solution

--- submissions/latestincyber/day-01/ClickCounter.sol
+++ submissions/latestincyber/day-01/ClickCounter.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract ClickCounter {
-    uint256 public count;
+    uint256 public counter;
 
     function click() public {
-        counter++
+        counter++;
     }
 }
