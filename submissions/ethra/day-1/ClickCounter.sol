162d81d38cb2674b4614e140d066b954a0d20b56
ethrahere <ethra.here@gmail.com>
2026-03-03 15:08:01 +0530

    Add ClickCounter contract for Day 1 submission

---
+++ submissions/ethra/day-1/ClickCounter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract ClickCounter {
+
+    uint256 public counter;
+
+    function click() public {
+        counter += 1;
+    }
+}
