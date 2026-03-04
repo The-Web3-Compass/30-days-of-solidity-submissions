aa2377e8f8b6177e80ab23c51c3b0f180ca98746
Shambhavigantla <shambhavireddy229@gmail.com>
2026-03-04 17:39:19 +0530

    day1

---
+++ submissions/Shambhavigantla/clickCounter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract clickCounter {
+
+    uint256 number;
+    
+    function increment() public {
+        number+=1;
+    }
+
+}
