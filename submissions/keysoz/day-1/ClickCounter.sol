07a25c4eaef998633fcd6ba4647c47775a7db1f1
Keysoz <keysoz122@gmail.com>
2026-03-05 21:08:06 +0200

    Day one Challenge ClickCounter.sol

--- submissions/keysoz/day-1/ClickCounter.sol
+++ submissions/keysoz/day-1/ClickCounter.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+error ClickCounter__AlreadyZero();
+
 contract ClickCounter {
     uint256 public counter;
 
-    function counterFunction() public {
+    function increaseCounter() external {
         counter++;
+    }
+
+    function decreaseCounter() external {
+        if (counter == 0) revert ClickCounter__AlreadyZero();
+        counter--;
+    }
+
+    function resetCounter() external {
+        counter = 0;
+    }
+
+    function getCounterValue() external view returns (uint256) {
+        return counter;
     }
 }
