076d4d02aef0b15c6abe7ea79b3aabb2d56f1845
Mialy RATSIMBAZAFY <mialyratsimbazafy@macbook-pro-de-mialy-1.home>
2026-03-06 22:17:03 +0100

    code update

--- submissions/Mialy333/Day 5 - AdminOnly.sol
+++ submissions/Mialy333/Day 5 - AdminOnly.sol
@@ -13,5 +13,50 @@ contract AdminOnly{
         owner = msg.sender;
     }
 
-    
+    modifier onlyOwner(){
+        require (msg.sender == owner, "Access denied:  Only the owner perform this action");
+        _;
+    }
+
+    function addTreasure(uint256 amount) public onlyOwner{
+        treasureAmount += amount;
+    }
+
+    function approveWithdrawal(address recipient, uint256 amount) public onlyOwner{
+        require(amount <= treasureAmount, "Insufficient funds in the contract"); 
+        withdrawalAllowance[recipient] = amount;
+
+    }
+
+    function withdralTreasure(uint256 amount) public{
+        if(msg.sender == owner){
+            require(amount <= treasureAmount, "Insufficient funds in the contract");
+            treasureAmount-= amount; 
+            return;
+        }
+
+        uint256 allowance = withdrawalAllowance[msg.sender];
+
+        require(allowance > 0, "You do not have any treasure allowance");
+        require(!hashWithdrawn[msg.sender], "You have already withdrawn your treasury");
+        require(allowance <= treasureAmount, "Not enough treasure in the chest");
+        require(amount <= allowance, "Not enough allowance for withdrawal");
+
+        hasWithdrawn[msg.sender] = true;
+        treasureAmount -= amount;
+        withdrawalAllowance[msg.sender] = 0;
+    }
+
+    function resetWithdrawalStatus(address user) public onlyOwner{
+        hasWithdrawn[user] = false;
+    }
+
+    function transferOwnership(address newOwner)public onlyOwner{
+        require(newOwner != address(0), "Invalid new owner");
+        owner = newOwner;
+    }
+
+    function getTreasure() public view onlyOwner returns(uint256){
+        return treasureAmount;
+    }
 }
