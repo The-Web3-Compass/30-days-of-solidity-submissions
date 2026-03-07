4a07e559cdc4d4fbadfd5ce2db191442626d4f03
Daniel Cueto <danielcuetorrico@gmail.com>
2026-03-07 14:59:37 -0400

    added day 6

---
+++ submissions/daniel/day-6/EthPiggyBank.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.30;
+
+contract EthPiggyBank {
+
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+    
+    function withdraw(uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance.");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
