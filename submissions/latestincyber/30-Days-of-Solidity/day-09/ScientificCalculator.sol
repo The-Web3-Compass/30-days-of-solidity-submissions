3d5d017bb358c0f300220640de17f88aafe946f3
doryoysterpie <ilbricconemarketing@gmail.com>
2026-03-10 14:15:03 -0400

    day 09 update v2

--- submissions/latestincyber/30-Days-of-Solidity/day-09/ScientificCalculator.sol
+++ submissions/latestincyber/30-Days-of-Solidity/day-09/ScientificCalculator.sol
@@ -5,20 +5,21 @@ contract ScientificCalculator {
     // advanced functions will go here - all the math that goes beyond the basics
 
     // power(base, exponent)
-    function power(uint256 base, uint256 exponent) public pure returns (uin256) {
+    function power(uint256 base, uint256 exponent) public pure returns (uint256) {
         if (exponent == 0) return 1;
         else return (base ** exponent);
     }
 
     // squareRoot(number)
-    function squareRoot(uint256 number) public pure returns (uint256) {
+    function squareRoot(int256 number) public pure returns (int256) {
         require(number >= 0, "Cannot calculate square root of negative number.");
         if (number == 0) return 0;
 
-        int256 result = number / 2;
-        for(uint256 i = 0; i < 10; i++) {
+        int256 result = number/2;
+        for(uint256 i = 0; i<10; i++) {
             result = (result + number / result) / 2;
         }
+
         return result;
     }
 }
