fddeb52b84772b69819395c34420c74b44dd0c14
doryoysterpie <ilbricconemarketing@gmail.com>
2026-03-11 12:10:44 -0400

    Update submissions/latestincyber/30-Days-of-Solidity/day-09/ScientificCalculator.sol
    Co-authored-by: codeant-ai[bot] <151821869+codeant-ai[bot]@users.noreply.github.com>

--- submissions/latestincyber/30-Days-of-Solidity/day-09/ScientificCalculator.sol
+++ submissions/latestincyber/30-Days-of-Solidity/day-09/ScientificCalculator.sol
@@ -11,15 +11,16 @@ contract ScientificCalculator {
     }
 
     // squareRoot(number)
-    function squareRoot(int256 number) public pure returns (int256) {
+    // squareRoot(number)
+    function squareRoot(uint256 number) public pure returns (uint256) {
         require(number >= 0, "Cannot calculate square root of negative number.");
         if (number == 0) return 0;
 
-        int256 result = number/2;
-        for(uint256 i = 0; i<10; i++) {
+        uint256 result = number;
+        for (uint256 i = 0; i < 10; i++) {
             result = (result + number / result) / 2;
         }
-
         return result;
+    }
     }
 }
