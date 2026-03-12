911926044fa589f8fdb5d90776a0a849fe36ba5c
raptor0929 <flaura.dev@gmail.com>
2026-03-12 03:48:10 -0300

    update solution day 2

--- submissions/raptor0929/Day02/SaveMyName.sol
+++ submissions/raptor0929/Day02/SaveMyName.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.13;
 
 contract SaveMyName {
     struct User {
-        bytes32 name;
-        bytes32 bio;
+        string name;
+        string bio;
     }
 
     mapping(address => User) public users;
 
     constructor() {}
 
-    function save(bytes32 _name, bytes32 _bio) external {
+    function save(string calldata _name, string calldata _bio) external {
         User storage user = users[msg.sender];
         user.name = _name;
         user.bio = _bio;
